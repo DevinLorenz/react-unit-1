b7713320833a8bb4a3551000b32dec2f957fe477
Devin Lorenz <devinlorenz.work@gmail.com>
2022-08-18 15:15:21 -0400

    Finished Unit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Square from './Square';
+const propVariable = 'This is a prop';
 
-function App() {
+
+
+const App = () => {
+  const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', '']);
+  const [player, setPlayer] = useState(true);
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    setPlayer(true)
+  };
+  function calculateWinner(arr) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            arr[a] &&
+            arr[a] === arr[b] &&
+            arr[a] === arr[c]
+        ) {
+            return `${arr[a]} won!`;
+        }
+    }
+    return "Who will win?";
+}  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <span>{calculateWinner(squares)}</span>
+      <div className="container">
+        {squares.map((val, index) => {
+          return (
+            <Square
+            setSquares={setSquares}
+            index={index}
+            squareValue={val}
+            squares={squares}
+            player={player}
+            setPlayer={setPlayer}
+            />
+            );
+          })}
+      </div>
+          <button onClick={handleClick}>Reset</button>
     </div>
   );
-}
+  
+};
+
+
 
 export default App;
